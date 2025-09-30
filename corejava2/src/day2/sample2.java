37a086fd60530730fb6dedc07b335db33367168c
Dell <Dell@Pavithra>
2025-09-30 10:35:45 +0530

    This is a sample2 program

---
+++ corejava2/src/day2/sample2.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("Main Method");
+		main(10);  
+	}
+	public static void main(int n) {
+		System.out.println("Overloaded Main Method"+n);
+	}
+
+}
