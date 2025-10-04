2721cbf4c4b47b7466541372d72dbc276eaedc99
Dell <Dell@Pavithra>
2025-10-04 17:46:40 +0530

    Day1

---
+++ DayOne/src/One/identifiersDemo.java
@@ -0,0 +1,15 @@
+package One;
+
+public class identifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
